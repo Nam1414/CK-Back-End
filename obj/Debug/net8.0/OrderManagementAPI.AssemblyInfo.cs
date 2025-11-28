e774273d36349b4bbe64c8ceeb2a20bbb4925c6e
Nguyễn Thanh Nam <thanhnam14112005@gmail.com>
2025-11-29 02:04:18 +0700

    Test database and device manager

--- obj/Debug/net8.0/OrderManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/OrderManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8efdfd28a18ee1816aeca8f40b9e37016e54724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30411f457d203ae668217fecdf6b20c284edc505")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
